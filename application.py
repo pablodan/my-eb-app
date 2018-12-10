5f703aaf5ceb4c79e0c5e0b28344d5ad70d1d5e4
pablodan <pomeally10@gmail.com>
2018-12-09 23:03:09 -0500

    Update application.py

--- application.py
+++ application.py
@@ -110,7 +110,7 @@ welcome = """
 </head>
 <body id="sample">
   <div class="textColumn">
-    <h1>THis is working!!!</h1>
+    <h1>THis is working NOW!!!</h1>
     <p>Your first AWS Elastic Beanstalk Python Application is now running on your own dedicated environment in the AWS Cloud</p>
   </div>
   
